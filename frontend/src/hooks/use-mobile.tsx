5a18bd4b9932eb46841aff0af118fa4631b9e551
ishitasingh <isingh05439@gmail.com>
2026-03-24 23:48:00 +0530

    [fix] resolve build issue

---
+++ frontend/src/hooks/use-mobile.tsx
@@ -0,0 +1,9 @@
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
+    mql.addEventListener("change", onChange);
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    return () => mql.removeEventListener("change", onChange);
+  }, []);
+
+  return !!isMobile;
+}
